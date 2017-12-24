f3ff25917271592401b8c01a04f1520d5f9b7325
小灰灰 <793983383@qq.com>
2017-12-24 13:42:32 +0800

    Initial Commit

---
+++ DemoZJ/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoZJ
+//
+//  Created by Mac on 2017/12/24.
+//  Copyright © 2017年 袁书辉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DemoZJ/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DemoZJ
+//
+//  Created by Mac on 2017/12/24.
+//  Copyright © 2017年 袁书辉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
